6e7bbb5b473b2e5e2c1cf3821c095c54de765bd6
Omair Javed <omairj7@gmail.com>
2021-04-26 21:03:07 -0400

    Hover over games to outline and show title

--- src/home/components/GameItem.tsx
+++ src/home/components/GameItem.tsx
@@ -3,17 +3,28 @@ import './GameItem.scss';
 import { Game } from '../GameScroll';
 
 type MyProps = { game: Game };
-type MyState = {};
+type MyState = { isHovering: boolean };
 class GameItem extends React.Component<MyProps, MyState> {
   constructor(props: any) {
     super(props);
+    this.state = {
+      isHovering: false
+    };
+  }
+
+  setHoverState(hovering: boolean) {
+    this.setState({
+      isHovering: hovering
+    });
   }
 
   render() {
     return (
       <div className="GameItem">
-        <p className="Game-title">{this.props.game.title}</p>
-        <div className="Game-logo">
+        <p className={"Game-title" + (this.state.isHovering ? '' : ' hovering')}>{this.props.game.title}</p>
+        <div className="Game-logo"
+          onMouseEnter={() => this.setHoverState(true)}
+          onMouseLeave={() => this.setHoverState(false)}>
           <img src={this.props.game.coverImgUrl} className="Logo" alt="logo" />
         </div>
       </div>
