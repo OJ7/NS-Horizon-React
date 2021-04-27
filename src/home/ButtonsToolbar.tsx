ed85b185065507406f93fea683d7d79cf1a1d9be
Omair Javed <omairj7@gmail.com>
2021-04-26 22:40:11 -0400

    Hover over menu buttons to outline and show title

--- src/home/ButtonsToolbar.tsx
+++ src/home/ButtonsToolbar.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import MenuButton from './components/MenuButton';
+import MenuButtonItem from './components/MenuButtonItem';
 import './ButtonsToolbar.scss';
 
-function ButtonsToolbar() {
-  return (
-    <div className="Buttons-toolbar">
-      <MenuButton></MenuButton>
-      <MenuButton></MenuButton>
-      <MenuButton></MenuButton>
-      <MenuButton></MenuButton>
-      <MenuButton></MenuButton>
-      <MenuButton></MenuButton>
-      <MenuButton></MenuButton>
-    </div>
+export type MenuButton = { title: string, color: string }
+type MyProps = {};
+type MyState = { buttons: MenuButton[] };
+class ButtonsToolbar extends React.Component<MyProps, MyState> {
+  constructor(props: MyProps) {
+    super(props);
+    this.state = {
+      buttons: [
+        { title: 'Nintendo Switch Online', color: '', },
+        { title: 'News', color: '', },
+        { title: 'Nintendo eShop', color: '', },
+        { title: 'Album', color: '', },
+        { title: 'Controllers', color: '', },
+        { title: 'System Settings', color: '', },
+        { title: 'Sleep Mode', color: '', },
+      ]
+    };
+  }
 
-  );
+  render() {
+    return (
+      <div className="Buttons-toolbar">
+        {this.state.buttons.map(button =>
+          <MenuButtonItem button={button} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default ButtonsToolbar;
---
+++ src/home/components/MenuButtonItem.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './MenuButtonItem.scss';
+import { MenuButton } from '../ButtonsToolbar';
+
+type MyProps = { button: MenuButton };
+type MyState = { isHovering: boolean };
+class MenuButtonItem extends React.Component<MyProps, MyState> {
+  constructor(props: MyProps) {
+    super(props);
+    this.state = {
+      isHovering: false
+    };
+  }
+
+  setHoverState(hovering: boolean) {
+    this.setState({
+      isHovering: hovering
+    });
+  }
+
+  render() {
+    return (
+      <div className="Menu-button-item" >
+        <span className="Button-container"
+          onMouseEnter={() => this.setHoverState(true)}
+          onMouseLeave={() => this.setHoverState(false)}>
+        </span>
+        <p className={"Button-title" + (this.state.isHovering ? '' : ' hidden')}>{this.props.button.title}</p>
+      </div>
+    );
+  }
+}
+
+export default MenuButtonItem;
